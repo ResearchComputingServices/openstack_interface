f8ad1a31921e0eab6aec4d1a4e6507d7cf332c90
Nicholi Shiell <nicholishiell@gmail.com>
2025-09-24 09:16:47 -0400

    added ability to switch project

--- openstack_interface/openstack_interface.py
+++ openstack_interface/openstack_interface.py
@@ -6,6 +6,9 @@ from neutronclient.v2_0 import client as neutronclient
 from glanceclient import client as glanceclient
 from keystoneauth1 import loading
 from keystoneauth1 import session as keystone_session
+from keystoneclient.v3 import client as keystone_client
+
+from pprint import pprint
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # OpenStack Nova and Glance API Version and Credentials
@@ -52,6 +55,9 @@ class OpenStackInterface:
         self.neutron = None
         self._init_neutron_client()
 
+        self.keystone = None
+        self._init_keystone_client()
+
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     def _get_creds(self):
@@ -94,6 +100,7 @@ class OpenStackInterface:
         loader = loading.get_plugin_loader('password')
         auth = loader.load_from_options(**creds)
 
+        # TODO: I think we should be able to get rid of this?
         sess = keystone_session.Session(auth=auth,verify=os.environ['OS_CACERT'])
 
         self.nova = novaclient.Client(NOVA_API_VERSION, **creds, cacert=os.environ['OS_CACERT'])
@@ -112,6 +119,46 @@ class OpenStackInterface:
         sess = keystone_session.Session(auth=auth,verify=os.environ['OS_CACERT'])
 
         self.glance = glanceclient.Client("2", session=sess)
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def _init_keystone_client(self):
+        """
+        Initialize the Keystone client with the credentials.
+        """
+        creds = self._get_creds()
+
+        loader = loading.get_plugin_loader('password')
+        auth = loader.load_from_options(**creds)
+
+        sess = keystone_session.Session(auth=auth,verify=os.environ['OS_CACERT'])
+
+        self.keystone = keystone_client.Client(session=sess)
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def _switch_project(self, project_name: str):
+        """
+        Switch the Nova client to a different project.
+        """
+        creds = self._get_creds()
+        creds['project_name'] = project_name
+
+        loader = loading.get_plugin_loader('password')
+        auth = loader.load_from_options(**creds)
+
+        # TODO: I think we should be able to get rid of this?
+        sess = keystone_session.Session(auth=auth,verify=os.environ['OS_CACERT'])
+
+        self.nova = novaclient.Client(NOVA_API_VERSION, **creds, cacert=os.environ['OS_CACERT'])
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def get_projects(self):
+        """
+        Get the list of projects from the Keystone client.
+        """
+        return self.keystone.projects.list()
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -134,6 +181,23 @@ class OpenStackInterface:
         Get the list of virtual machines from the Nova client.
         """
         return self.nova.servers.list()
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def get_network_id(self,
+                       faculty_name : str):
+        """
+        Get the network ID for a given faculty name.
+        """
+
+        network_id = '41117794-0b4c-4dd3-8f2b-7d9bb458e968'  # default to rcs network
+
+        for network in self.neutron.list_networks()['networks']:
+            if network['name'].lower() == faculty_name.lower():
+                network_id = network['id']
+                break
+
+        return network_id
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -190,14 +254,20 @@ class OpenStackInterface:
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    def assosciate_floating_ip(self, vm):
+    def assosciate_floating_ip(self, vm, network_id):
 
-        floating_ip = self.get_floating_ips()["floatingip"]
-        floating_ip_id = floating_ip["id"]
-        floating_ip_address = floating_ip["floating_ip_address"]
+        try:
+            floating_ip = self.neutron.create_floatingip({'floatingip': {'floating_network_id': network_id}})
+        except Exception as e:
+            raise ValueError(f"Failed to allocate floating IP: {e}")
 
-        time.sleep(10)
+        if floating_ip:
+            floating_ip_id = floating_ip['floatingip']["id"]
+            floating_ip_address = floating_ip['floatingip']["floating_ip_address"]
+
         # Wait for the VM to be in ACTIVE state
+        time.sleep(10)
+
         ports = self.neutron.list_ports(device_id=vm.id)
 
         for port in ports['ports']:
@@ -206,43 +276,46 @@ class OpenStackInterface:
             # Assign the floating IP to the port
             try:
                 self.neutron.update_floatingip(floating_ip_id, {'floatingip': {'port_id': port_id}})
-                print(f"Assigning floating IP {floating_ip_address} to port {port_id}")
             except Exception as e:
                 print(f"Error assigning floating IP {floating_ip_address} to port {port_id}: {e}")
+
+        return floating_ip_address
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     def create_vm(self,
+                  project_name : str,
                   hostname : str,
                   flavour,
                   image,
                   networks : list):
 
+        self._switch_project(project_name)
+
         #  Prepare user data for the VM (this is the initial setup script)
-        script_file_path = '/home/nickshiell/vm-setup-script.sh'
+        # TODO: remove this hard coding
+        script_file_path = '/home/sysadmin/setup-script.sh'
 
         # Read the script file and use it as user_data
         with open(script_file_path, 'r') as f:
             user_data = f.read()
 
         # create the VM using the Nova client
-        print(f"Creating VM with hostname: {hostname}, flavor: {flavour.name}, image: {image.name}, networks: {networks}")
-
         try:
             vm = self.nova.servers.create(  name=hostname,
                                             image=image,
                                             flavor=flavour,
-                                            key_name='openstack_key',
+                                            key_name='newmaster',
                                             nics=networks,
                                             userdata=user_data)
         except Exception as e:
             raise ValueError(f"Failed to create VM: {e}")
 
         # Associate a floating IP to the VM
-        print(f"VM {hostname} created with ID: {vm.id}")
-        self.assosciate_floating_ip(vm)
+        # self.assosciate_floating_ip(vm, networks[0]['net-id'])
+        floating_ip =self.assosciate_floating_ip(vm, 'bb005c60-fb45-481a-97fb-f746033e1c5d')
 
-        return vm
+        return vm, floating_ip
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -262,19 +335,18 @@ class OpenStackInterface:
 
         return image
 
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def get_server(self,
+                   vm_id : str):
+
+        return self.nova.servers.get(vm_id)
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    def get_floating_ips(self):
-        """_summary_
+    def get_vm_hypervisor_name(self,
+                               vm_id : str):
 
-        Returns:
-            _type_: _description_
-        """
-        for network_id in self.get_networks_ids():
+        return self.get_server(vm_id).to_dict().get("OS-EXT-SRV-ATTR:host")
 
-            try:
-                floating_ip = self.neutron.create_floatingip({'floatingip': {'floating_network_id': network_id}})
-                return floating_ip
-            except Exception as e:
-                print(f"Error creating floating IP for network {network_id}: {e}")
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
